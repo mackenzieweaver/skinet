8f0d45683699ca8d8c4d733e5429724f5b43cd9f
Mack <macknz7@gmail.com>
2021-02-10 13:46:26 -0800

    Eager load navigational properties

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -8,37 +8,44 @@ namespace Infrastructure.Data
 {
     public class ProductRepository : IProductRepository
     {
+        // CONSTRUCTOR
         private readonly StoreContext _context;
         public ProductRepository(StoreContext context)
         {
             _context = context;
         }
 
+        // BRANDS
         public async Task<ProductBrand> GetBrandByIdAsync(int id)
         {
             return await _context.ProductBrands.FindAsync(id);
         }
-
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
         {
             return await _context.ProductBrands.ToListAsync();
         }
 
+        // PRODUCTS
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p => p.Id == id);
         }
-
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
         }
 
+        // TYPES
         public async Task<ProductType> GetProductTypeByIdAsync(int id)
         {
             return await _context.ProductTypes.FindAsync(id);
         }
-
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
         {
             return await _context.ProductTypes.ToListAsync();
