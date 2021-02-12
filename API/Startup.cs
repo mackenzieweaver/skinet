1ecc63e2c6952deeed08a3a8fb1b3e483e3707ad
Mack <macknz7@gmail.com>
2021-02-12 12:44:56 -0800

    Startup cleanup by adding new folder / classes and extending IServiceProvider and IApplicationBuilder

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,12 @@
-using System.Linq;
-using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -34,50 +29,17 @@ namespace API
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection"))
             );
 
-            // Repository pattern - abstract data access away from controllers
-            services.AddScoped<IProductRepository, ProductRepository>();
-
-            // generic repository
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
             // add automapper
             services.AddAutoMapper(typeof(MappingProfiles));
 
-            // for /badrequest/actionthatdoesntexist
-            services.Configure<ApiBehaviorOptions>(options => 
-            {
-                options.InvalidModelStateResponseFactory = actionContext => 
-                {
-                    var errors = actionContext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
+            services.AddApplicationServices();
 
-                    var errorResponse = new ApiValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
-
-            services.AddSwaggerGen(c => 
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkiNet API", Version = "V1" });
-            });
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            // if (env.IsDevelopment())
-            // {
-                // app.UseDeveloperExceptionPage();
-                // app.UseSwagger();
-                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            //}
-            
+        {            
             app.UseMiddleware<ExceptionMiddleware>();
 
             // don't have an endpoint to match request
@@ -91,9 +53,8 @@ namespace API
 
             app.UseAuthorization();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1"); });
-
+            app.UseSwaggerDocumentation();
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
