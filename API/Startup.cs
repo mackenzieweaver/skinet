de37141b9c86c10b365cafe0da2c2729c3687eea
Mack <macknz7@gmail.com>
2021-02-11 17:59:52 -0800

    ApiException middleware includes stack trace

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using API.Helpers;
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -48,12 +49,17 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+            // if (env.IsDevelopment())
+            // {
+                // app.UseDeveloperExceptionPage();
                 // app.UseSwagger();
                 // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+            //}
+            
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            // don't have an endpoint to match request
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
