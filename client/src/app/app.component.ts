b6962f8be3ae12a7643521d4580ec89dafcefcfb
Mack <macknz7@gmail.com>
2021-02-13 19:29:25 -0800

    http module imported and getting data from api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {  
+  // properties
   title = 'Skinet';
+  products: any[];
+
+  constructor(private http: HttpClient){ }
+
+  // lifecycle hooks
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+      console.log(response);
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  // methods
+
 }
