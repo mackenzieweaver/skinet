6202e8d13797b4d5411f35ca40189e1897c8c61a
Mack <macknz7@gmail.com>
2021-02-13 20:24:19 -0800

    product and pagination interfaces

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,13 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {  
   // properties
   title = 'Skinet';
-  products: any[];
+  products: IProduct[];
 
   constructor(private http: HttpClient){ }
 
   // lifecycle hooks
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
       console.log(response);
       this.products = response.data;
     }, error => {
