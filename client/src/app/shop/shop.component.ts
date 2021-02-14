a927668f5d2c527d6959b047e3f9163a00819aba
Mack <macknz7@gmail.com>
2021-02-14 13:03:16 -0800

    IBrand, IType, real filter options
    Shop service takes brand and type ids
    sends filter into query string via HttpParams object
    Observable is returned
    RxJS .pipe method is used to map response to IPagination result

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brands';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,16 +11,56 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+
+  brandIdSelected: number;
+  typeIdSelected: number;
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts(): void {
     // subscribe to execute call to api
-    this.shopService.getProducts().subscribe(response => {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe(response => {
       this.products = response.data;
     }, error => {
       console.log(error);
     });
   }
 
+  getBrands(): void {
+    // subscribe to execute call to api
+    this.shopService.getBrands().subscribe(response => {
+      // concatenating new 'all' object with response
+      this.brands = [{ id: 0, name: 'All' }, ...response];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getTypes(): void {
+    // subscribe to execute call to api
+    this.shopService.getTypes().subscribe(response => {
+      // concatenating new 'all' object with response
+      this.types = [{ id: 0, name: 'All' }, ...response];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  onBrandSelected(brandId: number): void{
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number): void{
+    this.typeIdSelected = typeId;
+    this.getProducts();
+  }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,7 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IBrand } from '../shared/models/brands';
 import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,30 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(): Observable<IPagination> {
-    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  getProducts(brandId?: number, typeId?: number): Observable<IPagination> {
+    // params object to send in url
+    let params = new HttpParams();
+    if (brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if (typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+    // extract body when sending observe object
+    return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
+  }
+
+  getBrands(): Observable<IBrand[]>{
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes(): Observable<IType[]>{
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }
