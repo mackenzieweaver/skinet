922c98284b1c22395705cfc6e9e8ea12699a8de3
Mack <macknz7@gmail.com>
2021-02-16 13:00:56 -0800

    Validation Error UI

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -16,8 +16,12 @@ export class ErrorInterceptor implements HttpInterceptor {
                     if (error){
                         // validation error
                         if (error.status === 400){
-                            // toastr service
-                            this.toastr.error(error.error.message, error.error.statusCode);
+                            if (error.error.errors){
+                                throw error.error;
+                            } else {
+                                // toastr service
+                                this.toastr.error(error.error.message, error.error.statusCode);
+                            }
                         }
                         if (error.status === 401){
                             // toastr service
