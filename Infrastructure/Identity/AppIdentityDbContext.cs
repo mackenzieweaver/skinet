32572c5943e861b433068ce471f11f526ab8ab75
Mack <macknz7@gmail.com>
2021-02-25 12:12:26 -0800

    Adding Identity Db Context

---
+++ Infrastructure/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
+        // if you don't do this you get problems with Identity and the primary keys it's looking for
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
