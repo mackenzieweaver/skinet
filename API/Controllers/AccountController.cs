f2f2808a8f66b86fc752d685e63c7892bdc1e928
Mack <macknz7@gmail.com>
2021-02-25 18:06:02 -0800

    returning user Dto and an updateuseraddress method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
 using API.Extensions;
+using AutoMapper;
 using Core.Entities.Identity;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -17,12 +18,15 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
         public AccountController(UserManager<AppUser> userManager,
                                  SignInManager<AppUser> signInManager,
-                                 ITokenService tokenService)
+                                 ITokenService tokenService,
+                                 IMapper mapper)
         {
             _tokenService = tokenService;
+            _mapper = mapper;
             _userManager = userManager;
             _signInManager = signInManager;
         }
@@ -48,10 +52,23 @@ namespace API.Controllers
 
         [Authorize]
         [HttpGet("address")]
-        public async Task<ActionResult<Address>> GetUserAddress()
+        public async Task<ActionResult<AddressDto>> GetUserAddress()
         {
             var user = await _userManager.FindByClaimsPrincipalWithAddressAsync(HttpContext.User);
-            return user.Address;
+            return _mapper.Map<Address, AddressDto>(user.Address);
+        }
+
+        [Authorize]
+        [HttpPut("address")]
+        public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
+        {
+            var user = await _userManager.FindByClaimsPrincipalWithAddressAsync(HttpContext.User);
+            user.Address = _mapper.Map<AddressDto, Address>(address);
+            var result = await _userManager.UpdateAsync(user);
+
+            if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+            
+            return BadRequest("Problem updating the user");
         }
 
         [HttpPost("login")]
