bd31b132e9599bb1cc854ce2558f3b7ac38c1ca7
Mack <macknz7@gmail.com>
2021-03-05 13:50:49 -0800

    the order class!

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class Order : BaseEntity
+    {
+        public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, Address shipToAddress, DeliveryMethod deliverMethod, decimal subtotal)
+        {
+            BuyerEmail = buyerEmail;
+            ShipToAddress = shipToAddress;
+            DeliverMethod = deliverMethod;
+            OrderItems = orderItems;
+            Subtotal = subtotal;
+        }
+
+        public string BuyerEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public Address ShipToAddress { get; set; }
+        public DeliveryMethod DeliverMethod { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+        public decimal Subtotal { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public string PaymentIntentId { get; set; }
+
+        public decimal GetTotal()
+        {
+            return Subtotal + DeliverMethod.Price;
+        }
+    }
+}
